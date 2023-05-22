2e7b89356a8fc1e1e042f111a87c4ecff4f51e3c
lingllqs <lingllqs@163.com>
2023-05-22 22:54:05 +0800

    nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,16 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+    -- add gruvbox
+    { "ellisonleao/gruvbox.nvim" },
+    { "EdenEast/nightfox.nvim" },
+    { 'projekt0n/github-nvim-theme', version = 'v0.0.7' },
+    { "nxstynate/rosePine.nvim", priority = 1000 },
+    { "catppuccin/nvim", name = "catppuccin" },
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
+    -- Configure LazyVim to load gruvbox
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "tokyonight",
+        },
     },
-  },
 }
--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,11 +1,15 @@
 return {
     "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
     config = function ()
         vim.cmd([[
-            let g:mkdp_browser = '/usr/bin/chromium'
+            let g:mkdp_browser = 'chromium'
+            " nmap mk <Plug>MarkdownPreview
+            " nmap ms <Plug>MarkdownPreviewStop
+            nmap mk <Plug>MarkdownPreviewToggle
         ]])
     end,
-    keys = {
-        {"mk", "<cmd>MarkdownPreviewToggle<cr>", mode = {"n"}, desc = "markdown preview toggle"},
-    }
+    -- keys = {
+    --     {"<F5>", "Plug>MarkdownPreview", mode = {"n"}, desc = "markdown preview toggle"},
+    -- }
 }
