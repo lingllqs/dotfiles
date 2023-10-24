6084acd008d228e5d5205b42a1370a5c20002c18
lingllqs <lingllqs@163.com>
2023-10-24 17:43:18 +0800

    更新了一系列配置

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,20 +1,14 @@
 return {
-    {
-        'hardhackerlabs/theme-vim',
+  {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  },
+  {
+    { "ellisonleao/gruvbox.nvim", priority = 1000 },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
     },
-    {
-        "rebelot/kanagawa.nvim",
-    },
-    {
-        { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    },
-    {
-        { "ellisonleao/gruvbox.nvim", priority = 1000 },
-    },
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "tokyonight",
-      },
-    },
+  },
 }
