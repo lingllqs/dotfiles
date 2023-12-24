7febd0844572359e2c6a447558ebd87e98f7b378
lingllqs <lingllqs@163.com>
2023-12-24 16:19:04 +0800

    更新hyprland配置

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -5,7 +5,6 @@ wall_set = $HOME/Pictures/wallpaper/1.jpg
 wallpaper_set() {
 	swww img "$wall_set" \
 		--transition-bezier .43,1.19,1,.4 \
-		--transition-type "$xtrans" \
 		--transition-duration 0.7 \
 		--transition-fps 60 \
 		--invert-y \
