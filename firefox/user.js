ec7ecb5e9cedbf47e226583b61215c2f61632029
lingllqs <lingllqs@163.com>
2025-01-16 13:34:27 +0800

    firefox 配置

---
+++ firefox/user.js
@@ -0,0 +1,4 @@
+user_pref("layout.css.devPixelsPerPx", "1.0");
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.tabs.closeWindowWithLastTab", false); // last tab
+user_pref("browser.search.region", "US"); //region
