a3f20e0852b67eca7e8796dfdce0473c65c59b7d
jqms <hanjiangxueyesijing@gmail.com>
2025-12-03 12:04:02 +0800

    neovim conf

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -7,38 +7,15 @@ return {
 		spec = {
 			{
 				mode = { "n", "x" },
-				{ "<leader><tab>", group = "tabs" },
 				{ "<leader>c", group = "Code" },
-				{ "<leader>d", group = "debug" },
-				{ "<leader>dp", group = "profiler" },
-				{ "<leader>f", group = "file/find" },
-				{ "<leader>g", group = "git" },
-				{ "<leader>gh", group = "hunks" },
 				{ "<leader>q", group = "quit/session" },
 				{ "<leader>s", group = "Search" },
 				{ "<leader>u", group = "Ui" },
-				{ "<leader>x", group = "Diagnostics/quickfix" },
 				{ "[", group = "prev" },
 				{ "]", group = "next" },
 				{ "g", group = "goto" },
-				{ "gs", group = "surround" },
 				{ "z", group = "fold" },
-				{ "<leader>m", group = "[F]ormat and [T]ranslate" },
-				{
-					"<leader>b",
-					group = "buffer",
-					expand = function()
-						return require("which-key.extras").expand.buf()
-					end,
-				},
-				{
-					"<leader>w",
-					group = "windows",
-					proxy = "<c-w>",
-					expand = function()
-						return require("which-key.extras").expand.win()
-					end,
-				},
+				{ "<leader>m", group = "Format and Translate" },
 				-- better descriptions
 				{ "gx", desc = "Open with system app" },
 			},
