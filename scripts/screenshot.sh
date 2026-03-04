30dd0e64d990fc49de16eb7c4c1d85af6434a4ac
jqms <hanjiangxueyesijing@gmail.com>
2026-03-04 10:32:22 +0800

    截图

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -6,7 +6,7 @@ fi
 
 swpy_dir="$HOME/.config/swappy"
 save_dir="${2:-$XDG_PICTURES_DIR/screenshots}"
-save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
+save_file=$(date +'%y-%m-%d_%H:%M:%S.png')
 temp_screenshot="/tmp/screenshot.png"
 
 mkdir -p $save_dir
@@ -18,21 +18,22 @@ function print_error
 	cat <<"EOF"
     ./screenshot.sh <action>
     ...valid actions are...
-        p : print all screens
-        s : snip current screen
-        m : print focused monitor
+        a : print all screens
+        r : snip current screen
+        m : print current screen
+        c : print and cpoy to clipboard
 EOF
 }
 
 case $1 in
-p) # 截取所有屏幕并编辑
-	grimblast copysave screen $temp_screenshot && swappy -f $temp_screenshot ;;
-s) # 选取一个矩形区域或者鼠标点击截取一个窗口区域并编辑
-	grimblast --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot ;;
+a) # 截取所有屏幕并编辑
+	grim $temp_screenshot && satty -f $temp_screenshot ;;
+r) # 选取一个矩形区域或者鼠标点击截取一个窗口区域并编辑
+	grim -g "$(slurp)" $temp_screenshot && satty -f $temp_screenshot ;;
 m) # 截取当前屏幕并编辑
-	grimblast copysave output $temp_screenshot && swappy -f $temp_screenshot ;;
+	grim -o eDP-1 $temp_screenshot && satty -f $temp_screenshot ;;
 c) # 截取一个矩形区域或者鼠标点击截取一个窗口区域并复制到剪切板
-    grimblast --freeze copy area $temp_screenshot ;;
+	grim -g "$(slurp)" - | wl-copy ;;
 *) # 无效参数则打印提示信息
 	print_error ;;
 esac
@@ -42,5 +43,6 @@ rm "$temp_screenshot"
 
 # 如果保存了截图则弹窗提示
 if [ -f "$save_dir/$save_file" ]; then
-	dunstify "t1" -a "saved in $save_dir" -i "$save_dir/$save_file" -r 91190 -t 2200
+	# dunstify "t1" -a "saved in $save_dir" -i "$save_dir/$save_file" -r 91190 -t 2200
+	notify-send "save a screenshot"
 fi
